3db0f22663c31c33f3ca5b97d115bcacac1bb852
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-31 11:55:04 +0300

    final implementation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,32 @@
+#!/usr/bin/python3
 
+"""
+Pascal triangle implemention 
+"""
+
+
+def pascal_traingle(n):
+    """
+    returns the list of the numbers forming the pascal
+    triangle
+    """
+    if n <= 0:
+        return []
+
+    pascal_traingle = [0] * n
+
+    for i in range(n):
+        # defining a row and filling the first and the last index with 1
+        new_row = [0] * (i + 1)
+        new_row[0] = 0
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_traingle[i - 1][j]
+                b = pascal_traingle[i - 1][j - 1]
+                new_row[j] = a + b
+
+            pascal_traingle[i] = new_row
+
+    return pascal_traingle
